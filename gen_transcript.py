232160bdb2a571e16e04941543a047c9ddebdcea
theSLWayne <hiranhasanka@gmail.com>
2023-05-03 21:45:09 +0530

    Argument verificaion

--- gen_transcript.py
+++ gen_transcript.py
@@ -1,5 +1,6 @@
 from functions import generate_transcript_from_audio
 import argparse
+import os
 
 def init_args():
     """
@@ -48,3 +49,30 @@ def strToBool(value: str) -> bool:
             "Unsupported value found for --save_audio"
         )
     
+def validate_args(args):
+    """
+    
+    Validate arguments
+
+    Args:
+        args: Arguments
+
+    Returns:
+        None
+    """
+
+    # Check for either a video file path or an audio file path
+    assert (args.video_path or args.audio_path), "Need to provide a value for either --video_path or --audio_path"
+
+    if args.video_path:
+        # Check for validity of the video path
+        assert os.path.exists(args.video_path), "Provided video path does not exist."
+        # Make sure the file is a "MP4" file
+        assert os.path.basename(args.video_path).split('.')[-1] == "mp4", "The path is not an accepted video file."
+
+    if args.audio_path:
+        # check the validity of the audio path
+        assert os.path.exists(args.audio_path), "Provided audio_path does not exist."
+        # Make sure the file is a "WAV" file
+        assert os.path.basename(args.audio_path).split('.')[-1] == "wav", "The path is not an accepted audio file."
+
